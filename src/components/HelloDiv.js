136d127aa554b32c48787c22b7bee612a30a1daa
Steven McCarther <smccar5676@aol.com>
2021-01-13 14:33:25 -0500

    added fragment logic to HellDiv.js

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function HelloDiv() {
+  return [
+      <h1> Hello ! my name is Steven </h1>,
+      <h1> Oakland California </h1>
+  ]
+}
+
+export default HelloDiv;
